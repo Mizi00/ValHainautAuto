a8f57f9689f40b418189d1dd752b2c8b2bd9b1ae
Mizi00 <toufikboukichou.sio@gmail.com>
2024-01-19 17:32:58 +0100

    Start project "navbar" and setup project

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,12 @@
+<header class="header">
+    <div class="contain-logo"><a href="#" class="link-logo"><img class="logo-val" src="logo_val.png" alt=""></a>
+    <h1 class="title"><a href="#">VAL HAINAUT AUTO</a></h1></div>
+    <ul class="main-nav">
+        <li><a href="#">Qui somme Nous ?</a></li>
+        <li><a href="#">Voiture</a></li>
+        <li><a href="#">Réservation</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+    <div class="button-sidebar" id="button-sidebar"><a><i class="fa-solid fa-bars"></i></a></div>
+</header>
+<div class="header-space"></div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('body')
+<x-navbar/>
+
+@endsection
