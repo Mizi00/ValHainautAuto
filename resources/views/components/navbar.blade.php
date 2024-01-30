065f650bb777c6f32e86cc1ac62a3cd2230d878c
Mizi00 <toufikboukichou.sio@gmail.com>
2024-01-30 14:11:26 +0100

    inial commit project

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,11 @@
+<header class="header">
+    <h1 class="logo"><a href="#">Valhainaut <span>Auto</span></a></h1>
+    <ul class="main-nav">
+        <li><a href="#">Qui suis-je ?</a></li>
+        <li><a href="#">Nos services</a></li>
+        <li><a href="#">Véhicules</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+    <div class="button-sidebar" id="button-sidebar"><a><i class="fa-solid fa-bars"></i></a></div>
+</header>
+<div class="header-space"></div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('body')
+<x-navbar />
+
+<section class="hero">
+    <div class="hero-inner">
+        <h1>VALHAINAUT AUTO</h1>
+        <h4>Concessionnaire. Mécanique automobile. Service carte grise. Depuis 2015</h4>
+        <a class="button-hero" href="#">COMMENCER</a>
+    </div>
+</section>
+
+@endsection
