5a63e2a560834515af562cd0d7d468e39100ece8
Mizi00 <toufikboukichou.sio@gmail.com>
2024-02-13 14:14:53 +0100

    login fini

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
 @section('body')
-<x-navbar/>
+<x-navbar />
 
-<div class="login">
-    <div class="contain-login">
-        <h3>Se Connecter</h3>
-        <form action="">
+<div class="container-login">
+    <div class="login">
+        <h1>Se Connecter</h1>
+        <form action="" class="login-form   ">
             <input type="text" name="email" id="email" placeholder="Ex: Joe@exemple.fr">
             <input type="password" name="password" id="password" placeholder="Mot de passe ...">
             <input type="submit" value="Connexion" id="button" name="button">
         </form>
     </div>
-
 </div>
+
 @endsection
