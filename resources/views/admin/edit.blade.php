22ded4be756063fbc62fcfee6ef080f4ef04c996
Mizi00 <toufikboukichou.sio@gmail.com>
2024-02-20 12:56:57 +0100

    menu carburant ok

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -8,21 +8,56 @@
             <h1>Éditeur d'annonce</h1>
             <p>Annonce n°{{ $annonce->id }} ➜ {{ $annonce->titre }}</p>
         </div>
-
         <div class="inner-edit-annonce-text">
             <form action="" method="post">
-                <label for="">Nom / Titre: <input type="text" value="{{ $annonce->titre }}"></label>
-                <label for="">Prix: <input type="number" value="{{ $annonce->prix }}"></label>
-                <label for="">Modèle: <input type="text" value="{{ $annonce->modele }}"></label>
-                <label for="">Année: <input type="text" value="{{ $annonce->annee }}"></label>
-                <label for="">C.V: <input type="number" min="0" value="{{ $annonce->chevaux }}"></label>
-                <label for="">Type carburant: <input type="text" value="{{ $annonce->typeFuel }}"></label>
-                <!--<select name="type" id="">
-                    <option value="{{ $annonce->typeFuel }}">{{ $annonce->typeFuel }}</option>
-                    <option value=""></option>
-                </select>-->
-                <label for="">URL annonce: <input type="url" value="{{ $annonce->url }}"></label>
-                <label for="">Description: <textarea name="" id="" cols="30" rows="10">{{ $annonce->description }}</textarea></label>
+                @method('patch')
+                @csrf
+                <div class="div-form">
+                    <label for="">Nom / Titre:</label>
+                    <input type="text" value="{{ $annonce->titre }}">
+                </div>
+                <div class="div-form">
+                    <label for="">Prix:</label>
+                    <input type="number" value="{{ $annonce->prix }}">
+                </div>
+                <div class="div-form">
+                    <label for="">Modèle:</label>
+                    <input type="text" value="{{ $annonce->modele }}">
+                </div>
+                <div class="div-form">
+                    <label for="">Année:</label>
+                    <input type="text" value="{{ $annonce->annee }}">
+                </div>
+                <div class="div-form">
+                    <label for="">C.V:</label>
+                    <input type="number" min="0" value="{{ $annonce->chevaux }}">
+                </div>
+                <div class="div-form">
+                    <label for="">Type carburant:</label>
+                    <select name="type" id="">
+                        <option value="{{ $annonce->typeFuel }}">{{ $annonce->typeFuel }}</option>
+                        @if($annonce->typeFuel !='Essence')
+                        <option value="Essence">Essence</option>
+                        @endif
+                        @if($annonce->typeFuel !='Diesel')
+                        <option value="Diesel">Diesel</option>
+                        @endif
+                        @if($annonce->typeFuel !='Éléctrique')
+                        <option value="Éléctrique">Éléctrique</option>
+                        @endif
+                        @if($annonce->typeFuel !='Hybride')
+                        <option value="Hybride">Hybride</option>
+                        @endif
+                    </select>
+                </div>
+                <div class="div-form">
+                    <label for="">URL annonce:</label>
+                    <input type="url" value="{{ $annonce->url }}">
+                </div>
+                <div class="div-form">
+                    <label for="">Description:</label>
+                    <textarea name="" id="" cols="30" rows="10">{{ $annonce->description }}</textarea>
+                </div>
             </form>
 
         </div>
--- resources/views/client/voiture.blade.php
+++ resources/views/client/voiture.blade.php
@@ -14,7 +14,7 @@
         @forelse($annonces as $annonce)
         <li class="cards-item">
             <div class="card">
-                <div class="card-image"><img src="logo-val.png" alt=""></div>
+                <div class="card-image"><img src="{{ asset('img/logo-val.png') }}" alt=""></div>
                 <div class="card-content">
                     <div>
                         <h2 class="card-title">{{ $annonce->titre }}</h2>
