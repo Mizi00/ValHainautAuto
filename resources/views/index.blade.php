f72f32b9d6ed7e2fa69eabf9b209ee0ce00f72e5
Mizi00 <toufikboukichou.sio@gmail.com>
2024-03-01 20:43:06 +0100

    service update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,9 @@
     <div class="hero-inner">
         <h1>VAL HAINAUT AUTO</h1>
         <h4>Fait toujours le suivi de ses clients. Vous êtes prioritaires.</h4>
-        <a class="button-hero-button" href="#propos-id"><div class="button-hero">Voir Plus<i class="fa-solid fa-arrow-down"></i></div></a>
+        <a class="button-hero-button" href="#propos-id">
+            <div class="button-hero">Voir Plus<i class="fa-solid fa-arrow-down"></i></div>
+        </a>
     </div>
 </section>
 
@@ -18,9 +20,13 @@
     <div class="propos-inner" id="propos-id">
         <h1>Qui Sommes-Nous ?</h1>
         <div class="propos-text">
-            Quam ob rem ut ii qui superiores sunt submittere se debent in amicitia, sic quodam modo inferiores extollere. Sunt enim quidam qui molestas amicitias faciunt, cum ipsi se contemni putant; quod non fere contingit nisi iis qui etiam contemnendos se arbitrantur; qui hac opinione non modo verbis sed etiam opere levandi sunt.
+            <p>
+                Bienvenue chez <strong>Val Hainaut Auto</strong>, votre partenaire automobile de confiance dans la région ! Nous sommes fiers de vous offrir une expérience complète, allant de la vente de véhicules d'occasion de qualité à la reprise avantageuse de votre ancien véhicule. Notre équipe dévouée se spécialise également dans la réparation méticuleuse de véhicules, assurant ainsi la longévité et la performance de votre investissement automobile.</p>
 
-            Haec dum oriens diu perferret, caeli reserato tepore Constantius consulatu suo septies et Caesaris ter egressus Arelate Valentiam petit, in Gundomadum et Vadomarium fratres Alamannorum reges arma moturus, quorum crebris excursibus vastabantur confines limitibus terrae Gallorum.
+            <p>Chez <strong>Val Hainaut Auto</strong>, nous comprenons que chaque client est unique, c'est pourquoi nous proposons un service personnalisé et professionnel pour la création rapide de cartes grises. Simplifiez vos démarches administratives avec notre assistance experte.</p>
+
+            <p>Faites confiance à notre expertise éprouvée et à notre engagement envers la satisfaction client. Val Hainaut Auto, là où la qualité et le service se rencontrent sur la route de votre tranquillité d'esprit automobile. Explorez notre inventaire, profitez de nos services et découvrez une nouvelle dimension dans le monde automobile.</p>
+
         </div>
     </div>
 </section>
@@ -37,16 +43,20 @@
         </div>
         <div class="service-inner">
             <i class="fa-solid fa-screwdriver-wrench"></i>
-            <h1>Service n°2</h1>
+            <h1>Mécanique</h1>
             <div class="service-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam aspernatur sapiente officia debitis dolores vitae, perspiciatis atque at, in, quos iure corrupti quisquam aperiam fugiat cum quae iusto quasi omnis.
+                <p>
+                    Val Hainaut Auto prend soin de votre voiture avec expertise. De la mécanique à la carrosserie, notre service de réparation assure une qualité sans compromis. Roulez en toute confiance, car votre satisfaction est notre priorité absolue.
+                </p>
             </div>
         </div>
         <div class="service-inner">
             <i class="fa-regular fa-credit-card" id="icon_card"></i>
-            <h1>Service n°3</h1>
+            <h1>Carte Grise</h1>
             <div class="service-text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque porro quo vitae quisquam! Minima in ipsa, quos, voluptatum quidem quisquam repudiandae laboriosam aperiam blanditiis quasi fuga officiis ex deserunt dolorem?
+                <p>
+                    Simplifiez vos démarches administratives avec Val Hainaut Auto. Notre service de création de cartes grises vous offre une expérience sans tracas.
+                </p>
             </div>
         </div>
     </div>
@@ -69,7 +79,7 @@
                 <iframe src="https://www.youtube.com/embed/R10Yc3DnZKw" title="Vidéo de présentation   Val Hainaut Auto" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
         </div>
-    </div> 
+    </div>
     <div class="map-inner">
         <h1>Nous retrouvez ...</h1>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2546.2088665013307!2d3.508659776550962!3d50.344009571572016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2edaf6d3cb8cb%3A0x7001af2a1c62840e!2sVAL%20HAINAUT%20AUTO!5e0!3m2!1sfr!2sfr!4v1706715589431!5m2!1sfr!2sfr" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
