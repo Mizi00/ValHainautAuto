b52835a4792492a3a8304cb45e44a048b655265f
Mizi00 <toufikboukichou2@gmail.com>
2024-03-06 21:53:20 +0100

    commit

--- resources/views/client/contact.blade.php
+++ resources/views/client/contact.blade.php
@@ -11,13 +11,13 @@
     <form action="{{ route('contact') }}" method="post">
         @csrf
         <div class="contact-np">
-            <label for="">Prénom: <input type="text" name="prenom" id="prenom" placeholder="Ex: John"></label>
-            <label for="">Nom: <input type="text" name="nom" id="nom" placeholder="Ex: Doe"></label>
+            <label for="">Prénom: <input type="text" name="prenom" id="prenom" placeholder="Ex: John" value="{{ old('prenom') }}"></label>
+            <label for="">Nom: <input type="text" name="nom" id="nom" placeholder="Ex: Doe" value="{{ old('nom') }}"></label>
         </div>
-        <label for="">Adresse Mail: <input type="text" name="mail" id="email" placeholder="Ex: john@doe.fr"></label>
-        <label for="">Numéro de téléphone: <input type="text" name="numero" id="number" placeholder="Ex: 0623431233"></label>
+        <label for="">Adresse Mail: <input type="text" name="mail" id="email" placeholder="Ex: john@doe.fr" value="{{ old('mail') }}"></label>
+        <label for="">Numéro de téléphone: <input type="text" name="numero" id="number" placeholder="Ex: 0623431233" value="{{ old('numero') }}"></label>
         <label for="">Votre Message :
-            <textarea name="message" id="msg" placeholder="Ecrire ici ..." style="height:200px"></textarea>
+            <textarea name="message" id="msg" placeholder="Ecrire ici ..." style="height:200px">{{ old('message') }}</textarea>
         </label>
         <div class="form-error">
             @if ($errors->any())
