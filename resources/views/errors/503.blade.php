8f110cf1891fc07463087f4877aa9ed59f53e823
Mizi00 <toufikboukichou2@gmail.com>
2024-03-09 19:59:50 +0100

    modif

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EN MAINTENANCE</title>
+    <style>
+        body{
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 0;
+        }
+        h1 {
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 55px;
+        }
+        @media screen and (max-width: 1024px) {
+            h1 {
+                font-size: 30px;
+            }
+        }
+        @media screen and (max-width: 450px) {
+            h1 {
+                font-size: 20px;
+            }
+        }
+    </style>
+</head>
+<body>
+    <h1>SITE EN MAINTENANCE...</h1>
+</body>
+</html>		
